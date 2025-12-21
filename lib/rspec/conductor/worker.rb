b6cf8f6cb740349dccd0297c2e1d737b86589b66
Mark Abramov <me@markabramov.me>
2025-12-21 21:22:55 +0400

    Do not include default spec path in LOAD_PATH twice

--- lib/rspec/conductor/worker.rb
+++ lib/rspec/conductor/worker.rb
@@ -72,7 +72,7 @@ module RSpec
         @default_path = RSpec.configuration.default_path || "spec"
         @default_full_path = File.expand_path(@default_path, Conductor.root)
 
-        if Dir.exist?(@default_full_path)
+        if Dir.exist?(@default_full_path) && !$LOAD_PATH.include?(@default_full_path)
           $LOAD_PATH.unshift(@default_full_path)
         end
 
