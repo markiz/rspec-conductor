0e51f512d658cebce40a4ee709c97bd3732320b3
Mark Abramov <me@markabramov.me>
2026-01-10 00:04:49 +0400

    Extract rspec extensions to a separate file

---
+++ lib/rspec/conductor/ext/rspec.rb
@@ -0,0 +1,13 @@
+# RSpec doesn't provide us with a good way to handle before/after suite hooks,
+# doing what we can here
+class RSpec::Core::Configuration
+  def __run_before_suite_hooks
+    RSpec.current_scope = :before_suite_hook if RSpec.respond_to?(:current_scope=)
+    run_suite_hooks("a `before(:suite)` hook", @before_suite_hooks) if respond_to?(:run_suite_hooks)
+  end
+
+  def __run_after_suite_hooks
+    RSpec.current_scope = :after_suite_hook if RSpec.respond_to?(:current_scope=)
+    run_suite_hooks("an `after(:suite)` hook", @after_suite_hooks) if respond_to?(:run_suite_hooks)
+  end
+end
